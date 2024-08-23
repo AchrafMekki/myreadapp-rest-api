bd361f0c30d693350d06730f6bde96332b905c3a
Achraf <Achraf.BenMekk@dci-student.org>
2024-08-23 10:01:28 +0200

    author  serializer 'name' read-only

--- apps/book/serializer/tag.py
+++ apps/book/serializer/tag.py
@@ -1,27 +1,28 @@
 from rest_framework import serializers
 from apps.book.models import Tag
+import re
 
 
 class TagSerializer(serializers.Serializer): 
     
-    name = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField(read_only=True)
     
     def name(self,obj):
-        return obj.name.capitalize()
+        return obj.name.title()
 
     
     def validate_tag_name(self,value):
-        if value in ['%','!','@','#','$','%','^','&','*']:
-            raise serializers.ValidationError(
-                "Tag nameshould not contain special characters like '%','!','@','#','$','%','^','&','*'"
-                )
-        return value
-
-        # if re.search(r'[!@#$%^&*]', value):
-        #     raise serializers.ValidationError("Tag name should not contain special characters like !@#$%^&*")
+        # if value in ['%','!','@','#','$','%','^','&','*']:
+        #     raise serializers.ValidationError(
+        #         "Tag nameshould not contain special characters like '%','!','@','#','$','%','^','&','*'"
+        #         )
         # return value
+
+        if re.search(r'[!@#$%^&*]', value):
+            raise serializers.ValidationError("Tag name should not contain special characters like !@#$%^&*")
+        return value
 
     class Meta:
         model = Tag
         fields = "__all__"  # ('id', 'name')
-        read_only_fields = ('id')
+        read_only_fields = ('id',)
